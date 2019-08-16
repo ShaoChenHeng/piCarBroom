17eda1c9b98c410a0d75a01761499cbb495c7712
shaochenheng <1010716589@qq.com>
2019-08-16 22:42:57 +0800

    standardized some code and improved functions

--- car/motor/motor_ctl.py
+++ car/motor/motor_ctl.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-import time
+import time 
 import sys
 
 
@@ -13,14 +13,11 @@ IN3 = 27 #rightside -
 IN4 = 22 #rightside +
 
 #使能接口
-enableA = 20
-enableB = 21
+enable = 20
 
 speed  = 25
-GPIO.setup(enableA, GPIO.OUT)
-GPIO.setup(enableB, GPIO.OUT)
-motorA = GPIO.PWM(enableA,100)
-motorB = GPIO.PWM(enableB,100)
+GPIO.setup(enable, GPIO.OUT)
+motor = GPIO.PWM(enable,100)
 
 
 def init(): 
@@ -29,8 +26,7 @@ def init():
     GPIO.setup(IN2, GPIO.OUT)  
     GPIO.setup(IN3, GPIO.OUT)
     GPIO.setup(IN4, GPIO.OUT)
-    motorA.start(0)
-    motorB.start(0)
+    motor.start(0)
     
    
 def pause():
@@ -46,20 +42,18 @@ def up():
     GPIO.output(IN3, GPIO.LOW)
     GPIO.output(IN1, GPIO.LOW)
     time.sleep(0.1)
-    motorA.ChangeDutyCycle(25)
-    motorB.ChangeDutyCycle(25)
+    motor.ChangeDutyCycle(25)
     pause()
     #GPIO.output(IN1,GPIO.LOW)
 
 #顺时针旋转
 def CW():
-    GPIO.output(IN4, GPIO.LOW)
     GPIO.output(IN2, GPIO.HIGH)
     GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
     GPIO.output(IN1, GPIO.LOW)
     time.sleep(0.1)
-    motorA.ChangeDutyCycle(25)
-    motorB.ChangeDutyCycle(25)
+    motor.ChangeDutyCycle(40) #转速根据小车载重决定
     pause()
 
 # 逆时针旋转
@@ -69,8 +63,7 @@ def CCW():
     GPIO.output(IN4, GPIO.HIGH)
     GPIO.output(IN2, GPIO.LOW)
     time.sleep(0.1)
-    motorA.ChangeDutyCycle(25)
-    motorB.ChangeDutyCycle(25)
+    motor.ChangeDutyCycle(40)
     pause()
 
 def down():
@@ -78,46 +71,43 @@ def down():
     GPIO.output(IN2, GPIO.LOW)
     GPIO.output(IN3, GPIO.HIGH)
     GPIO.output(IN1, GPIO.HIGH)
-    motorA.ChangeDutyCycle(25)
-    motorB.ChangeDutyCycle(25)
+    motor.ChangeDutyCycle(25)
     time.sleep(0.1)
     pause()
 
-def turn_right1():
+def turnRight1():
     GPIO.output(IN1, GPIO.LOW)
     GPIO.output(IN2, GPIO.LOW)
     GPIO.output(IN3, GPIO.LOW)
     GPIO.output(IN4, GPIO.HIGH)
-    motorA.ChangeDutyCycle(25)
-    motorB.ChangeDutyCycle(25)    
+    motor.ChangeDutyCycle(25)
     time.sleep(0.1)
     pause()
 
-def turn_right2():
+def turnRight2():
     GPIO.output(IN1, GPIO.LOW)
     GPIO.output(IN2, GPIO.LOW)
     GPIO.output(IN3, GPIO.HIGH)
     GPIO.output(IN4, GPIO.LOW)
+    motor.ChangeDutyCycle(25)
     time.sleep(0.1)
     pause()
 
-def turn_left1():
+def turnLeft1():
     GPIO.output(IN1, GPIO.LOW)
     GPIO.output(IN2, GPIO.HIGH)
     GPIO.output(IN3, GPIO.LOW)
     GPIO.output(IN4, GPIO.LOW)
-    motorA.ChangeDutyCycle(25)
-    motorB.ChangeDutyCycle(25)
+    motor.ChangeDutyCycle(25)
     time.sleep(0.1)
     pause()
 
-def turn_left2():
+def turnLeft2():
     GPIO.output(IN1, GPIO.HIGH)
     GPIO.output(IN2, GPIO.LOW)
     GPIO.output(IN3, GPIO.LOW)
     GPIO.output(IN4, GPIO.LOW)
-    motorA.ChangeDutyCycle(25)
-    motorB.ChangeDutyCycle(25)
+    motor.ChangeDutyCycle(25)
     time.sleep(0.1)
     pause()
 
