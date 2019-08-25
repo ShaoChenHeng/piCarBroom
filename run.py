f170414f1c1f1feff2bc13297c0cf6a51b142635
shaochenheng <1010716589@qq.com>
2019-08-25 12:15:16 +0800

    sort out the run file

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 import tkinter as tk
-from car.motor.motor_ctl import *
 from car.infra.infraControl import *
-from car.algo import *
-
+from car.keyboard.keyboardInput import *
 
 #待添加
     #LED交互
@@ -10,38 +8,6 @@ from car.algo import *
     #电机通电开关
     #地图建立算法
     #路径规划主算法
-def FloodFill():
-    pass
- 
-#根据键盘输入使小车做出相应运动
-#利用tkinter得到键盘输入
-def keyInput(event):
-    print("pressed",repr(event.char))
-    key_press = event.char
-    print(keyPress.lower())
-
-    if keyPress.lower() == 'w':
-        up()
-    elif keyPress.lower() == 's':
-        down()
-    elif keyPress.lower() == 't':      #停止按钮
-        stop()
-    elif keyPress.lower() == 'e':
-        turnRight1()
-    elif keyPress.lower() == 'c':
-        turnRight2()
-    elif keyPress.lower() == 'q':
-        turnLeft1()
-    elif keyPress.lower() == 'z':
-        turnLeft2()
-    elif keyPress.lower() == 'a':
-        CW()
-    elif keyPress.lower() == 'd':
-        CCW()
-    elif keyPress.lower() == 'm':
-        simpleGo()    
-    elif keyPress.lower() == '\x1b':   #退出按钮
-        exit()
 
 init()
 initInfra()
