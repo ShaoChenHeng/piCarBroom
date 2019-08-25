382b8f6f88866aac75817d17e7369f62b2e25a0f
shaochenheng <1010716589@qq.com>
2019-08-25 23:27:55 +0800

    use a new func to get kb-input

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from car.infra.infraControl import *
 from car.keyboard.keyboardInput import *
+from car.motor.motor_ctl import *
+from car.algorithm.algo import *
 
 #待添加
     #LED交互
@@ -9,13 +11,46 @@ from car.keyboard.keyboardInput import *
     #地图建立算法
     #路径规划主算法
 
+
+#根据键盘输入使小车做出相应运动
+#利用tkinter得到键盘输入
+def keyInput():
+    keyPress = kbhit()
+    if ( len(keyPress) > 0 ):
+        print("pressed %s"%keyPress)
+
+    if  keyPress == 'w':
+        up()
+    elif keyPress == 's':
+        down()
+    elif keyPress == 't':      #停止按钮
+        stop()
+    elif keyPress == 'e':
+        turnRight1()
+    elif keyPress == 'c':
+        turnRight2()
+    elif keyPress == 'q':
+        turnLeft1()
+    elif keyPress == 'z':
+        turnLeft2()
+    elif keyPress == 'a':
+        CW()
+    elif keyPress == 'd':
+        CCW()
+    elif keyPress == 'm':
+        simpleGo()    
+    elif keyPress == '\x1b':   #退出按钮
+        exit()
+
 init()
 initInfra()
 
-command = tk.Tk()
-command.bind('<KeyPress>',keyInput)
+#command = tk.Tk()
+#command.bind('<KeyPress>',keyInput)
 InfraControl()
-command.mainloop()
+#command.mainloop()
+while True:
+    keyInput()
 
 GPIO.cleanup()
 
